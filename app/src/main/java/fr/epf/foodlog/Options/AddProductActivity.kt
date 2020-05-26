5ccb909ece4d1a2b162d3a333dacbe8c1dc987f3
Remi Beltramini <remi.beltramini@gmail.com>
2020-05-26 13:42:07 +0200

    Integration date scanner
    Bug  : surfaceview

--- app/src/main/java/fr/epf/foodlog/Options/AddProductActivity.kt
+++ app/src/main/java/fr/epf/foodlog/Options/AddProductActivity.kt
@@ -1,17 +1,26 @@
 package fr.epf.foodlog.Options
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.SurfaceHolder
 import android.view.View
 import android.widget.*
 import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.vision.CameraSource
+import com.google.android.gms.vision.Detector
+import com.google.android.gms.vision.text.TextBlock
+import com.google.android.gms.vision.text.TextRecognizer
 import com.google.zxing.integration.android.IntentIntegrator
 import com.google.zxing.integration.android.IntentResult
 import fr.epf.foodlog.R
@@ -23,6 +32,7 @@ import kotlinx.android.synthetic.main.stock_dialog.view.*
 import kotlinx.coroutines.runBlocking
 import java.time.LocalDate
 import java.util.*
+import kotlin.properties.Delegates
 
 class AddProductActivity : AppCompatActivity() {
 
@@ -30,13 +40,37 @@ class AddProductActivity : AppCompatActivity() {
     private var mDisplayDate: TextView? = null
     private var mDateSetListener: DatePickerDialog.OnDateSetListener? = null
 
+    private var mCameraSource by Delegates.notNull<CameraSource>()
+    private var textRecognizer by Delegates.notNull<TextRecognizer>()
+    private lateinit var textdetecter :String
+    private val PERMISSION_REQUEST_CAMERA = 100
+
+
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_add_product)
+
+        layout_date_scanner.visibility=(RelativeLayout.INVISIBLE)
+
 
         val levels = resources.getStringArray(R.array.level_array)
         val spinner = findViewById<Spinner>(R.id.level_spinner)
+
+        button_scan_date.setOnClickListener{
+            layout_add_product.visibility=(RelativeLayout.INVISIBLE)
+
+          //  surface_camera_preview.visibility=(RelativeLayout.VISIBLE)
+            startCameraSource()
+            layout_date_scanner.visibility=(RelativeLayout.VISIBLE)
+        }
+        button_back_scan_date.setOnClickListener {
+            mCameraSource.stop()
+            layout_add_product.visibility=(RelativeLayout.VISIBLE)
+            layout_date_scanner.visibility=(RelativeLayout.INVISIBLE)
+        }
+
 
         if (spinner != null) {
             val adapter = ArrayAdapter(this,
@@ -193,8 +227,8 @@ class AddProductActivity : AppCompatActivity() {
 
         fab_scan.setOnClickListener{
             run {
-                IntentIntegrator(this@AddProductActivity).initiateScan();
-            }
+                IntentIntegrator(this@AddProductActivity).initiateScan()
+                }
         }
 
     }
@@ -248,6 +282,147 @@ class AddProductActivity : AppCompatActivity() {
         } else {
             super.onActivityResult(requestCode, resultCode, data)
         }
+    }
+
+    //
+
+    private fun startCameraSource() {
+
+        //  Create text Recognizer
+        textRecognizer = TextRecognizer.Builder(this).build()
+
+        if (!textRecognizer.isOperational) {
+            Log.d("syst","Dependencies are downloading....try after few moment")
+            return
+        }
+        //  Init camera source to use high resolution and auto focus
+            mCameraSource = CameraSource.Builder(applicationContext, textRecognizer)
+            .setFacing(CameraSource.CAMERA_FACING_BACK)
+            .setRequestedPreviewSize(1280, 1024)
+            .setAutoFocusEnabled(true)
+            .setRequestedFps(2.0f)
+            .build()
+
+        surface_camera_preview.holder.addCallback(object : SurfaceHolder.Callback {
+            override fun surfaceChanged(p0: SurfaceHolder?, p1: Int, p2: Int, p3: Int) {
+                Log.d("syst","surface changed")
+            }
+
+            override fun surfaceDestroyed(p0: SurfaceHolder?) {
+                mCameraSource.stop()
+            }
+
+            @SuppressLint("MissingPermission")
+            override fun surfaceCreated(p0: SurfaceHolder?) {
+                Log.d("syst","surfaceCreated")
+                try {
+                    if (isCameraPermissionGranted()) {
+                        mCameraSource.start(surface_camera_preview.holder)
+                    } else {
+                        requestForPermission()
+                    }
+                } catch (e: Exception) {
+                    //toast("Error:  ${e.message}")
+                }
+            }
+        })
+
+        textRecognizer.setProcessor(object : Detector.Processor<TextBlock> {
+            override fun release() {}
+
+            override fun receiveDetections(detections: Detector.Detections<TextBlock>) {
+                Log.d("syst","receiveDetections")
+                val items = detections.detectedItems
+
+                if (items.size() <= 0) {
+                    return
+                }
+
+                    val stringBuilder = StringBuilder()
+                    for (i in 0 until items.size()) {
+                        val item = items.valueAt(i)
+                        stringBuilder.append(item.value)
+                        stringBuilder.append("\n")
+                    }
+                    textdetecter = stringBuilder.toString()
+                    val date=detectDate(textdetecter)
+                Log.d("detection",textdetecter)
+                    if(date===""){
+
+                    }else{
+                        //toast(date)
+                    }
+            }
+        })
+    }
+
+    fun isCameraPermissionGranted(): Boolean {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) ==
+                PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestForPermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), PERMISSION_REQUEST_CAMERA)
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode != PERMISSION_REQUEST_CAMERA) {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+            return
+        }
+
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (isCameraPermissionGranted()) {
+                mCameraSource.start(surface_camera_preview.holder)
+                Log.d("syst","surface_camera_preveiw.holder")
+            } else {
+                // toast("Permission need to grant")
+                Log.d("syst","Permission need to grant")
+                finish()
+            }
+        }
+    }
+
+
+    fun detectDate(text:String):String{
+
+        // var date:String="02/04/2019"
+        val date = text
+
+        val pattern=Regex("\\d{2}/\\d{2}/\\d{4}")
+        //"^\\d{4}-\\d{2}-\\d{2}$"
+        // \d{4}/\d{2}/\d{2}
+        val date2: Date
+        if(pattern.containsMatchIn(date))
+        {
+            val matchResult=pattern.find(date)
+            //    Toast.makeText(this,"Date :"+matchResult!!.value, Toast.LENGTH_SHORT).show()
+            return  matchResult!!.value
+        }else{
+            val pattern2=Regex("\\d{2}/\\d{2}/\\d{2}")
+            if(pattern2.containsMatchIn(date)) {
+                val matchResult=pattern2.find(date)
+                //   Toast.makeText(this,"Date :"+matchResult!!.value, Toast.LENGTH_SHORT).show()
+                return  matchResult!!.value
+            }
+        }
+
+        val pattern3=Regex("\\d{2}/\\d{2}")
+
+        if(pattern.containsMatchIn(date)){
+            val matchResult=pattern3.find(text)
+            return  matchResult!!.value
+
+        }
+
+        if(pattern.matches(text))
+        {
+            //  Toast.makeText(this, "true", Toast.LENGTH_SHORT).show()
+        }
+        //    val find=pattern.find(text)
+        return ""
+
     }
 
 
