de0ef2e3ba32a51634dcb3a53e427ec2db5c51a6
Remi Beltramini <remi.beltramini@gmail.com>
2020-05-27 02:28:34 +0200

    Ajout date scanner

--- app/src/main/java/fr/epf/foodlog/Options/AddProductActivity.kt
+++ app/src/main/java/fr/epf/foodlog/Options/AddProductActivity.kt
@@ -1,17 +1,26 @@
 package fr.epf.foodlog.Options
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.SurfaceHolder
 import android.view.View
 import android.widget.*
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.vision.CameraSource
+import com.google.android.gms.vision.Detector
+import com.google.android.gms.vision.text.TextBlock
+import com.google.android.gms.vision.text.TextRecognizer
 import com.google.zxing.integration.android.IntentIntegrator
 import com.google.zxing.integration.android.IntentResult
 import fr.epf.foodlog.R
@@ -23,6 +32,7 @@ import kotlinx.android.synthetic.main.stock_dialog.view.*
 import kotlinx.coroutines.runBlocking
 import java.time.LocalDate
 import java.util.*
+import kotlin.properties.Delegates
 
 class AddProductActivity : AppCompatActivity() {
 
@@ -30,20 +40,61 @@ class AddProductActivity : AppCompatActivity() {
     private var mDisplayDate: TextView? = null
     private var mDateSetListener: DatePickerDialog.OnDateSetListener? = null
 
+    private var mCameraSource by Delegates.notNull<CameraSource>()
+    private var textRecognizer by Delegates.notNull<TextRecognizer>()
+    private lateinit var textdetecter: String
+    private val PERMISSION_REQUEST_CAMERA = 100
+
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_product)
 
+        textRecognizer = TextRecognizer.Builder(this).build()
+        mCameraSource = CameraSource.Builder(applicationContext, textRecognizer)
+            .setFacing(CameraSource.CAMERA_FACING_BACK)
+            .setRequestedPreviewSize(1280, 1024)
+            .setAutoFocusEnabled(true)
+            .setRequestedFps(2.0f)
+            .build()
+
+        startCameraSource()
+
+        mCameraSource.stop()
+
+        layout_date_scanner.visibility = (RelativeLayout.INVISIBLE)
+
+
+        button_scan_date.setOnClickListener {
+            //  surface_camera_preview.visibility=(RelativeLayout.VISIBLE)
+            initCamera()
+            startCameraSource()
+            layout_date_scanner.visibility = (RelativeLayout.VISIBLE)
+            layout_add_product.visibility = (RelativeLayout.INVISIBLE)
+        }
+        button_back_scan_date.setOnClickListener {
+            mCameraSource.stop()
+            mCameraSource.release()
+            layout_add_product.visibility = (RelativeLayout.VISIBLE)
+            layout_date_scanner.visibility = (RelativeLayout.INVISIBLE)
+        }
+
         val levels = resources.getStringArray(R.array.level_array)
         val spinner = findViewById<Spinner>(R.id.level_spinner)
 
         if (spinner != null) {
-            val adapter = ArrayAdapter(this,
-                android.R.layout.simple_spinner_item, levels)
+            val adapter = ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_item, levels
+            )
             spinner.adapter = adapter
             spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-                override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
 
                 }
 
@@ -70,16 +121,16 @@ class AddProductActivity : AppCompatActivity() {
         mDateSetListener =
             DatePickerDialog.OnDateSetListener { datePicker, year, month, day ->
                 var month = month
-                var date_month : String = ""
-                var date_day : String = ""
+                var date_month: String = ""
+                var date_day: String = ""
                 month = month + 1
-                if ("${month}".length == 1){
+                if ("${month}".length == 1) {
                     date_month = "0$month"
                 } else {
                     date_month = "$month"
                 }
 
-                if ("${day}".length == 1){
+                if ("${day}".length == 1) {
                     date_day = "0$day"
                 } else {
                     date_day = "$day"
@@ -90,8 +141,7 @@ class AddProductActivity : AppCompatActivity() {
             }
 
 
-
-        var unite:String
+        var unite: String
         var NumUnite: Int = 0
         var stock: String = ""
         /*
@@ -104,7 +154,8 @@ class AddProductActivity : AppCompatActivity() {
             unite = unite_spinner.selectedItem as String
             val mDialogView = LayoutInflater.from(this).inflate(R.layout.stock_dialog, null)
             // Set a SeekBar change listener
-            mDialogView.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            mDialogView.seekBar.setOnSeekBarChangeListener(object :
+                SeekBar.OnSeekBarChangeListener {
                 override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
                     // Display the current progress of SeekBar
                     mDialogView.text_view_seekbar.text = "$i"
@@ -120,13 +171,12 @@ class AddProductActivity : AppCompatActivity() {
             })
 
             // Set le input stock
-            if (unite=="Portions"){
+            if (unite == "Portions") {
                 mDialogView.seekBar.setVisibility(View.VISIBLE);
                 mDialogView.text_view_seekbar.setVisibility(View.VISIBLE);
                 mDialogView.product_dialog_unite.setText("portions")
                 NumUnite = 2
-            }
-            else {
+            } else {
                 mDialogView.stock_edittext.setVisibility(View.VISIBLE);
                 mDialogView.product_dialog_unite.setText("grammes")
                 NumUnite = 1
@@ -136,21 +186,20 @@ class AddProductActivity : AppCompatActivity() {
                 .setView(mDialogView)
                 .setTitle("Ajout de la quantité")
             //show dialog
-            val  mAlertDialog = mBuilder.show()
+            val mAlertDialog = mBuilder.show()
 
             //add button click of custom layout
             mDialogView.dialogValiderBtn.setOnClickListener {
                 //dismiss dialog
                 mAlertDialog.dismiss()
                 //get text from EditTexts of custom layout
-                if (unite=="Portions"){
+                if (unite == "Portions") {
                     stock = mDialogView.text_view_seekbar.text.toString()
-                }
-                else {
+                } else {
                     stock = mDialogView.stock_edittext.text.toString()
                 }
                 //set the input text in TextView
-                if(stock != "")
+                if (stock != "")
                     Add_enterQuantite.setText(stock)
                 else
                     Add_enterQuantite.setText("Entrez la quantité")
@@ -167,10 +216,10 @@ class AddProductActivity : AppCompatActivity() {
         //                      AFFICHAGE LOGCAT
         add_product_button.setOnClickListener {
             val name = lastname_edittext.text.toString()
-            val type : String = level_spinner.selectedItem as String
+            val type: String = level_spinner.selectedItem as String
             val stockEntre = Add_enterQuantite.text.toString()
-            var typeProduct : Int = 0
-            when(type){
+            var typeProduct: Int = 0
+            when (type) {
                 "LEGUME" -> typeProduct = 2
                 "FRUIT" -> typeProduct = 1
                 "CEREALE" -> typeProduct = 3
@@ -191,7 +240,7 @@ class AddProductActivity : AppCompatActivity() {
             finish()
         }
 
-        fab_scan.setOnClickListener{
+        fab_scan.setOnClickListener {
             run {
                 IntentIntegrator(this@AddProductActivity).initiateScan();
             }
@@ -199,20 +248,28 @@ class AddProductActivity : AppCompatActivity() {
 
     }
 
-    private fun getServer(name : String, type : String, date : String, stock:String, unite:Int){
-        val service  = retrofit("https://foodlog.min.epf.fr/").create(ProductService::class.java)
+    private fun getServer(name: String, type: String, date: String, stock: String, unite: Int) {
+        val service = retrofit("https://foodlog.min.epf.fr/").create(ProductService::class.java)
         val pref = applicationContext.getSharedPreferences(
             "Foodlog",
             Context.MODE_PRIVATE
         )
         val token = pref.getString("token", null);
         runBlocking {
-            val result = service.postProduct("${token}", "${name}", "${type}","${date}", "${stock}", "$unite")
+            val result = service.postProduct(
+                "${token}",
+                "${name}",
+                "${type}",
+                "${date}",
+                "${stock}",
+                "$unite"
+            )
         }
     }
 
-    private fun callOFFTest(id: String){
-        val service  = retrofit("https://world.openfoodfacts.org/").create(OpenFoodFactsAPI::class.java)
+    private fun callOFFTest(id: String) {
+        val service =
+            retrofit("https://world.openfoodfacts.org/").create(OpenFoodFactsAPI::class.java)
 
         runBlocking {
             val result = service.loadAPIResponse(id)
@@ -220,7 +277,7 @@ class AddProductActivity : AppCompatActivity() {
             val quantity = result.product.quantity.filter { it.isDigit() }
             Add_enterQuantite.setText(quantity)
             val unity = result.product.quantity.filter { it.isLetter() }
-            if (unity == "g"){
+            if (unity == "g") {
                 unite_spinner.setSelection(0)
             } else {
                 unite_spinner.setSelection(1)
@@ -230,10 +287,11 @@ class AddProductActivity : AppCompatActivity() {
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        val result: IntentResult? = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        val result: IntentResult? =
+            IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
 
-        if(result != null){
-            if(result.contents != null){
+        if (result != null) {
+            if (result.contents != null) {
                 scannedResult = result.contents
                 val ids = scannedResult
                 //callOFF(ids)
@@ -250,6 +308,207 @@ class AddProductActivity : AppCompatActivity() {
         }
     }
 
+
+    private fun initCamera() {
+        textRecognizer = TextRecognizer.Builder(this).build()
+        mCameraSource = CameraSource.Builder(applicationContext, textRecognizer)
+            .setFacing(CameraSource.CAMERA_FACING_BACK)
+            .setRequestedPreviewSize(1280, 1024)
+            .setAutoFocusEnabled(true)
+            .setRequestedFps(2.0f)
+            .build()
+
+        Log.d("Cam","Camera init")
+    }
+
+    private fun startCameraSource() {
+
+        //  Create text Recognizer
+        // textRecognizer = TextRecognizer.Builder(this).build()
+
+
+        if (!textRecognizer.isOperational) {
+            Log.d("syst", "Dependencies are downloading....try after few moment")
+            return
+        }
+
+        surface_camera_preview.holder.addCallback(object : SurfaceHolder.Callback {
+            override fun surfaceChanged(p0: SurfaceHolder?, p1: Int, p2: Int, p3: Int) {
+                Log.d("syst", "surface changed")
+            }
+
+            override fun surfaceDestroyed(p0: SurfaceHolder?) {
+                mCameraSource.stop()
+            }
+
+            @SuppressLint("MissingPermission")
+            override fun surfaceCreated(p0: SurfaceHolder?) {
+                Log.d("syst", "surfaceCreated")
+                try {
+                    if (isCameraPermissionGranted()) {
+                        mCameraSource.start(surface_camera_preview.holder)
+                    } else {
+                        requestForPermission()
+                    }
+                } catch (e: Exception) {
+                    //toast("Error:  ${e.message}")
+                }
+            }
+        })
+
+        textRecognizer.setProcessor(object : Detector.Processor<TextBlock> {
+            override fun release() {}
+
+            override fun receiveDetections(detections: Detector.Detections<TextBlock>) {
+                Log.d("syst", "receiveDetections")
+                val items = detections.detectedItems
+
+                if (items.size() <= 0) {
+                    return
+                }
+
+                val stringBuilder = StringBuilder()
+                for (i in 0 until items.size()) {
+                    val item = items.valueAt(i)
+                    stringBuilder.append(item.value)
+                    stringBuilder.append("\n")
+                }
+                textdetecter = stringBuilder.toString()
+                val date = detectDate(textdetecter)
+                Log.d("detection", textdetecter.toString())
+                if (date === "" ) {
+                    Log.d("Cam",date.toString())
+
+                } else {
+                    Log.d("Cam",date.toString())
+                    Log.d("StopCam", "StopCam")
+
+                    mCameraSource.release()
+                    mCameraSource.stop()
+
+                    this@AddProductActivity.runOnUiThread(Runnable {
+                        layout_date_scanner.visibility = (RelativeLayout.INVISIBLE)
+                        layout_add_product.visibility = (RelativeLayout.VISIBLE)
+                    tvDate.text = date
+                    })
+
+
+                }
+            }
+        })
+    }
+
+    fun isCameraPermissionGranted(): Boolean {
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) ==
+                PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestForPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.CAMERA),
+            PERMISSION_REQUEST_CAMERA
+        )
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode != PERMISSION_REQUEST_CAMERA) {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+            return
+        }
+
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (isCameraPermissionGranted()) {
+                mCameraSource.start(surface_camera_preview.holder)
+            } else {
+                // toast("Permission need to grant")
+                Log.d("syst", "Permission need to grant")
+                finish()
+            }
+        }
+    }
+
+    fun detectDate(text: String): String {
+
+        //var date:String="02 04 2019" //(cas numero 1)
+        //var date:String="2019-04/03" //(cas numero 2)
+        // var date: String = "19-04.20" //(cas numero 3)
+        // var date:String="2014-02" // (cas numero 4)
+        // var date="06/2024" // (cas numero 5)
+        // var date="04/22" // (cas numero 6)
+
+        val a: String = "(/|.|\\s)" // = separator
+
+        val date = text
+        var pattern = Regex("\\d{2}$a\\d{2}$a\\d{4}")
+        if (pattern.containsMatchIn(date)) {
+            val matchResult = pattern.find(date)
+            val b = matchResult!!.value.replace(".", "-").replace("/", "-").replace("\\s", "-")
+            val y = b.substring(6)    // annee
+            val d = b.substring(0, 2)  // jours
+            val m = b.substring(3, 5)  // mois
+            return "$y-$m-$d"
+        } else {
+            pattern = Regex("\\d{4}$a\\d{2}$a\\d{2}")
+            if (pattern.containsMatchIn(date)) {
+                val matchResult = pattern.find(date)
+                return matchResult!!.value.replace(".", "-").replace("/", "-").replace("\\s", "-")
+            } else {
+                pattern = Regex("\\d{2}$a\\d{2}$a\\d{2}")
+                if (pattern.containsMatchIn(date)) {
+                    val matchResult = pattern.find(date)
+                    val b =
+                        matchResult!!.value.replace(".", "-").replace("/", "-").replace("\\s", "-")
+                    val d = b.substring(0, 2)
+                    val m = b.substring(3, 5)
+                    val y = "20" + b.substring(6)
+                    return "$y-$m-$d"
+                } else {
+                    pattern = Regex("\\d{4}$a\\d{2}")
+                    if (pattern.containsMatchIn(date)) {
+                        val matchResult = pattern.find(date)
+                        val b = matchResult!!.value.replace(".", "-").replace("/", "-")
+                            .replace("\\s", "-")
+                        val d = "25"
+                        val m = b.substring(5)
+                        val y = b.substring(0, 4)
+                        return "$y-$m-$d"
+                    } else {
+                        pattern = Regex("\\d{2}$a\\d{4}")
+                        if (pattern.containsMatchIn(date)) {
+                            val matchResult = pattern.find(date)
+                            val b = matchResult!!.value.replace(".", "-").replace("/", "-")
+                                .replace("\\s", "-")
+                            val d = "25"
+                            val m = b.substring(0, 2)
+                            val y = b.substring(3)
+                            return "$y-$m-$d"
+                        } else {
+                            pattern = Regex("\\d{2}$a\\d{2}")
+                            if (pattern.containsMatchIn(date)) {
+                                val matchResult = pattern.find(date)
+                                val b = matchResult!!.value.replace(".", "-").replace("/", "-")
+                                    .replace("\\s", "-")
+                                val d = "25"
+                                val m = b.substring(0, 2)
+                                val y = "20" + b.substring(3)
+                                return "$y-$m-$d"
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+
+        return ""
+
+    }
 
 
 }
