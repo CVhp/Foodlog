5383c542824013799b4da093b1252b135f7f82da
Remi Beltramini <remi.beltramini@gmail.com>
2020-06-01 16:14:03 +0200

    Redirection automatique
    Redirige automatiquement au dernier frigo ouvert

--- app/src/main/java/fr/epf/foodlog/ui/fridge/FridgeFragment.kt
+++ app/src/main/java/fr/epf/foodlog/ui/fridge/FridgeFragment.kt
@@ -2,6 +2,7 @@ package fr.epf.foodlog.ui.fridge
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -22,6 +23,20 @@ import kotlinx.coroutines.runBlocking
  */
 class FridgeFragment : Fragment() {
 
+    override fun onStart() {
+        val appContext = requireActivity().applicationContext
+        val pref = appContext.getSharedPreferences(
+            "Foodlog",
+            Context.MODE_PRIVATE
+        )
+        val fridge=pref.getInt("fridge",0)
+        if(fridge!=0){
+            Log.d("Fridge",fridge.toString())
+                val target=FridgeFragmentDirections.actionFridgeFragmentToNavListproduct(fridge)
+                Navigation.findNavController(requireView()).navigate(target)
+        }
+        super.onStart()
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -40,10 +55,11 @@ class FridgeFragment : Fragment() {
             Context.MODE_PRIVATE
         )
         val token = pref.getString("token", null);
+
             root.fab_fridge.setOnClickListener {
 
-           // val target=FridgeFragmentDirections.actionFridgeFragmentToAddFridgeFragment()
-            //    Navigation.findNavController(requireView()).navigate(target)
+            val target=FridgeFragmentDirections.actionFridgeFragmentToAddFridgeFragment()
+                Navigation.findNavController(requireView()).navigate(target)
 
             }
         runBlocking {
